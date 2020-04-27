de3e78a228339add3b7ed80fd24166607fcd317d
tonyhew <916390127@qq.com>
2020-04-27 13:03:49 +0800

    整理代码格式

--- src/components/home/Author.js
+++ src/components/home/Author.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Author extends Component {
-    render() {
-        return (
-            <div>Author</div>
-        )
-    }
+	render() {
+		return (
+			<div>Author</div>
+		)
+	}
 }
 
 export default Author
